de8d5770fc9c726d72c7e130a6c55435625bebcd
Alpha6849 <alphaprathamesh6849@gmail.com>
2025-12-11 17:05:22 +0530

    updated eda module

--- Phase2_Pipeline/pipeline/trainer.py
+++ Phase2_Pipeline/pipeline/trainer.py
@@ -48,3 +48,33 @@ class ModelTrainer:
         self.results = {}
         self.best_model = None
         self.best_score = -1
+        
+        
+    def prepare_data(self):
+        
+        logger.info("Preparing data for training...")
+
+        # classification :- encode target
+        if self.task_type == "classification":
+            from sklearn.preprocessing import LabelEncoder
+            self.label_encoder = LabelEncoder()
+            self.y = self.label_encoder.fit_transform(self.y)
+            logger.info("Target column encoded for classification.")
+        else:
+            self.label_encoder = None
+
+        # Train/Test split
+        self.X_train, self.X_test, self.y_train, self.y_test = train_test_split(
+            self.X, self.y, test_size=0.2, random_state=42
+        )
+
+        logger.info(
+            f"Data prepared. Train size: {self.X_train.shape}, Test size: {self.X_test.shape}"
+        )
+
+        return {
+            "train_shape": self.X_train.shape,
+            "test_shape": self.X_test.shape,
+            "task_type": self.task_type
+        }
+
